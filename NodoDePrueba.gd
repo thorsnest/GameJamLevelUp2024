309612875564a41d8529ae799d7bf0fe96ea2f1d
ToniSenpaii <avinaderc2001@gmail.com>
2024-07-02 22:05:24 +0200

    Hola

--- NodoDePrueba.gd
+++ NodoDePrueba.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("mi pito susio")
+	print("hola")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
